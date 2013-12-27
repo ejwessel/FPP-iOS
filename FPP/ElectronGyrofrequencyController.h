61a52a060cd3b6e72b6f5a1772f20e6abd1db836
Ethan Wessel <ejwessel@ethan-wessels-macbook.local>
2013-12-27 11:52:17 -0800

    Set up UI in storyboard for the first two options Created a new Class Electron Gyro Freq which has yet to be implemented.

---
+++ FPP/ElectronGyrofrequencyController.h
@@ -0,0 +1,13 @@
+//
+//  ElectronGyrofrequencyController.h
+//  FPP
+//
+//  Created by Ethan Wessel on 12/27/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElectronGyrofrequencyController : UIViewController
+
+@end
